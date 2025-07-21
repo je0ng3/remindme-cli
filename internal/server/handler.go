08c16346b8fb9af66f7cd7b74f98ffab03442bef
je0ng3 <jepark2934@gmail.com>
2025-07-21 16:15:23 +0900

    feat: implement schedule-based notification delivery

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -6,8 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"sync"
 	"text/tabwriter"
+	"time"
 
 	"github.com/google/uuid"
 	schedulepb "github.com/je0ng3/remindme-cli/api/proto/schedulepb"
@@ -35,6 +37,7 @@ func (s *ScheduleServer) AddSchedule(ctx context.Context, req *schedulepb.Schedu
 		return nil, errors.New("title is required")
 	}
 	id := uuid.New().String()
+	req.Id = id
 
 	file, err := os.OpenFile(s.csvFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -45,11 +48,11 @@ func (s *ScheduleServer) AddSchedule(ctx context.Context, req *schedulepb.Schedu
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	err = writer.Write([]string{id, req.Title, req.Datetime, req.Url, req.Memo})
+	err = writer.Write([]string{req.Id, req.Title, req.Datetime, req.Url, req.Memo})
 	if err != nil {
 		return nil, err
 	}
-
+	go s.WatchSchedule(req)
 	return &schedulepb.ScheduleResponse{Message: "Schedule added."}, nil
 }
 
@@ -81,12 +84,7 @@ func (s *ScheduleServer) ListSchedules(ctx context.Context, _ *schedulepb.Empty)
 		})
 	}
 
-	fmt.Println("Schedules:")
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(w, "ID\tTitle\tDatetime\tURL\tMemo")
-	for i, sch := range list {
-		fmt.Fprintf(w, "%d\t%s\t%s\t%s\t%s\n", i+1, sch.Title, sch.Datetime, sch.Url, sch.Memo)
-	}
 	w.Flush()
 
 	return &schedulepb.ScheduleList{Schedules: list}, nil
@@ -129,4 +127,104 @@ func (s *ScheduleServer) DeleteSchedule(ctx context.Context, req *schedulepb.Sch
 	}
 
 	return &schedulepb.ScheduleResponse{Message: "Schedule deleted."}, nil
+}
+
+
+func (s *ScheduleServer) WatchSchedule (req *schedulepb.ScheduleRequest) {
+	layout := "2006-01-02 15:04"
+	loc := time.Local
+	t, err := time.ParseInLocation(layout, req.Datetime, loc)
+	if err != nil {
+		fmt.Println("날짜 포맷 불일치:", err)
+	}
+
+	duration := time.Until(t)
+	if duration <= 0 {
+		return
+	}
+	
+	time.Sleep(duration)
+
+	if s.scheduleExists(req.Id) {
+		err = notification(req.Title, req.Memo, req.Url)
+		if err != nil {
+			fmt.Println("알림 전송 실패:", err)
+		}
+		err = s.deleteExpiredSchedule(req.Id)
+		if err != nil {
+			fmt.Println("일정 삭제 실패:", err)
+		}
+	}
+}
+
+func notification(title, memo, url string) error {
+	args := []string {"-title", title}
+	if memo != "" {
+		args = append(args, "-message", memo)
+	}
+	if url != "" {
+		args = append(args, "-open", url)
+	}
+	fmt.Println(args)
+	cmd := exec.Command("terminal-notifier", args...)
+	return cmd.Run()
+}
+
+
+func (s *ScheduleServer) deleteExpiredSchedule(id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	file, err := os.Open(s.csvFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+
+	var updatedRecords [][]string
+	for _, record := range records {
+		if record[0] != id {
+			updatedRecords = append(updatedRecords, record)
+		}
+	}
+
+	updatedFile, err := os.Create(s.csvFile)
+	if err != nil {
+		return err
+	}
+	defer updatedFile.Close()
+
+	writer := csv.NewWriter(updatedFile)
+	defer writer.Flush()
+	return writer.WriteAll(updatedRecords)
+}
+
+func (s *ScheduleServer) scheduleExists(id string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	file, err := os.Open(s.csvFile)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return false
+	}
+
+	for _, record := range records {
+		if record[0] == id {
+			return true
+		}
+	}
+	return false
 }
